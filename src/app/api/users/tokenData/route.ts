6bd798d1858c3a61a3e7d21c5064160babebfd73
yogyata-04 <yogyatasingh2002@gmail.com>
2025-08-06 14:43:44 +0530

    add get tokenData api route

---
+++ src/app/api/users/tokenData/route.ts
@@ -0,0 +1,21 @@
+//extracting data from token
+
+import { getDataFromToken } from "@/helpers/getDataFromToken";
+import { NextRequest,NextResponse } from "next/server";
+import User from "@/models/userModel";
+import { connect } from "@/dbConfig/dbConfig";
+
+connect();
+
+export async function GET(request:NextRequest){
+    try {
+        const userId=await getDataFromToken(request);
+        const user=await User.findOne({_id:userId}).select("-password -isAdmin") //I don't want password and isAdmin in response
+        return NextResponse.json({
+            message:"User found",
+            data:user
+        })
+    } catch (error:any) {
+        return NextResponse.json({error: error.message},{status:400});
+    }
+}
